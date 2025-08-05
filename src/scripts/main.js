fc336e14b8bb0186d1cfeb9b996ce393ac382d7c
Maxim <makson202007@gmail.com>
2025-08-05 13:40:23 +0300

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,19 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  for (const oneList in data) {
+    const listName = document.createElement('li');
+
+    listName.textContent = oneList;
+    element.appendChild(listName);
+
+    if (Object.keys(data[oneList]).length > 0) {
+      const listNode = document.createElement('ul');
+
+      createTree(listNode, data[oneList]);
+      listName.appendChild(listNode);
+    }
+  }
 }
 
 createTree(tree, food);
